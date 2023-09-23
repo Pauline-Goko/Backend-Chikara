4aef9ff8e8f5fd12950022bf4573e47cc5858501
Rachel-Kenyani <rachelkenyani@gmail.com>
2023-09-23 18:44:54 +0300

    fixed bugs

--- Chikara/Chikara/urls.py
+++ Chikara/Chikara/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("limit/", include("limit.urls")),
-    path("user/", include("user.urls"))
+    path("user/", include("user.urls")),
     path("vehicle/", include("vehicles.urls"))
 ]
