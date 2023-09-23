933c7e6a97fa45806380daaa0abecf383250bd0d
Pauline-Wanjiru <gokowanjiru@gmail.com>
2023-09-23 21:31:32 +0300

    hidden the db

--- Chikara/Chikara/settings.py
+++ Chikara/Chikara/settings.py
@@ -79,12 +79,12 @@ import os
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ecobasi',
-        'USER': 'admin',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': os.environ.get('DB_ENGINE'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     },
 }
 
