ad1ae503e1cdb34c2786b78820d66133660c5204
Rachel-Kenyani <rachelkenyani@gmail.com>
2023-09-23 19:05:12 +0300

    fixed bugs

--- Chikara/Chikara/settings.py
+++ Chikara/Chikara/settings.py
@@ -94,11 +94,7 @@ DATABASES = {
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT'),
-    },
-    'test': {
-        'ENGINE': os.environ.get('DB_ENGINE'),
-        'NAME': os.environ.get('DB_NAME'),
-    },
+    }
 }
 
 
