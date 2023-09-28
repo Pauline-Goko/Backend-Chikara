a5193225513cbe8b09511bc86bd6d39f6f1757db
rita-khaseyi <ritakhaseyi@gmail.com>
2023-09-28 16:47:33 +0300

    fixed bugs

--- Chikara/urls.py
+++ Chikara/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from rest_framework_swagger.views import get_swagger_view
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
